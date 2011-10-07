8ab18330c9bccf803b1ceb0058e36fe8f6d05dd6
Edwin de Jonge <edwindjonge@gmail.com>
2011-10-07 10:21:50 +0200

    changed to roxygen2 so build.bat and build.bash work again

--- roxygen.r
+++ roxygen.r
@@ -1,14 +1,14 @@
-library(roxygen)
+library(roxygen2)
 
 unlink('pkg/man', TRUE)
 
-roxygenize( 'pkg'
-          , roxygen.dir='pkg'
+setwd('pkg')
+roxygenize( '.'
+          , roxygen.dir='.'
           , copy.package=FALSE
           , unlink.target=TRUE
-          , use.Rd2 = TRUE
 		    )
 
-if (length(list.files('pkg/inst/doc')) == 0){
-   unlink( 'pkg/inst/doc', TRUE)   
+if (length(list.files('inst/doc')) == 0){
+   unlink( 'inst/doc', TRUE)   
 }

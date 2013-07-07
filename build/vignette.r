a7e94e2f0b7bac1bdb3fe7142d0236305f26240a
Martijn Tennekes <mtennekes@gmail.com>
2013-07-07 13:40:41 +0200

    add timings vignette

--- build/vignette.r
+++ build/vignette.r
@@ -2,7 +2,10 @@ library(knitr)
 
 setwd('../vignette')
 knit2pdf("tabplot-vignette.Rnw", "tabplot-vignette.tex")
+knit2pdf("tabplot-timings.Rnw", "tabplot-timings.tex")
 
 unlink("./pgk/inst/doc/tabplot-vignette.pdf")
+unlink("./pgk/inst/doc/tabplot-timings.pdf")
 shell("copy tabplot-vignette.pdf ..\\pkg\\inst\\doc")
+shell("copy tabplot-timings.pdf ..\\pkg\\inst\\doc")
 setwd('../build')
